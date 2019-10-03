b5387cf1bb56551b2463a265c9ff7e94ad028efd
Gaurav <gaurav.bamania@dal.ca>
2019-10-03 00:18:20 -0300

    adding more files

---
+++ 2nd_video/accessing interface info.py
@@ -0,0 +1,26 @@
+from device_info import ios_xe1
+from ncclient import manager
+from pprint import pprint
+import xmltodict
+
+netconf_filter = open('filter-ietf-interfaces.xml','r').read()
+
+with manager.connect(host= ios_xe1['address'],
+                     port= ios_xe1['port'],
+                     username= ios_xe1['username'],
+                     password= ios_xe1['password'],
+                     hostkey_verify= False) as m:
+
+    netconf_reply = m.get(netconf_filter)
+
+    #print(netconf_reply)
+
+    #processing the xml and store in useful dictionaries
+
+    intf_details = xmltodict.parse(netconf_reply.xml)['rpc-reply']['data']
+
+    #print(intf_details)
+
+    intf_config = intf_details['interfaces']['interface']
+
+    pprint(intf_config)
